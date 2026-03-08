04be38578b339736e9e087879a91fb2f74e5f90c
ZyadMohamed14 <zyadmuhammed05@gmail.com>
2026-03-08 23:43:16 +0200

    update routhes on app router

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,9 +1,11 @@
 import 'package:clinic_app/core/routes/routes.dart';
 import 'package:clinic_app/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:clinic_app/features/home/presentation/cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../features/auth/presentation/view/auth_screen.dart';
 import '../../features/auth/presentation/view/otp_verification_page.dart';
+import '../../features/clinic_details/presentation/cubit/clinic_details_cubit.dart';
 import '../../features/dashboard/dashboard.dart';
 import '../../features/clinic_details/presentation/view/clinic_details_screen.dart';
 import '../di/injection_container.dart' as di;
@@ -14,7 +16,6 @@ class AppRouter {
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-
       case Routes.auth:
         return MaterialPageRoute(
           builder: (_) => BlocProvider<AuthCubit>(
@@ -34,16 +35,28 @@ class AppRouter {
 
       case Routes.dashBoard:
         return MaterialPageRoute(
-          builder: (_) => const PatientHomeScreen(),
+          builder: (_) => MultiBlocProvider(
+            providers: [
+              BlocProvider<AuthCubit>(
+                create: (_) => di.sl<AuthCubit>(),
+              ),
+              BlocProvider<HomeCubit>(
+                create: (_) => di.sl<HomeCubit>()..initHome(),
+              ),
+            ],
+            child: const PatientHomeScreen(),
+          ),
         );
-
       case Routes.clinicDetails:
         final clinicId = settings.arguments as int?;
         if (clinicId == null) {
           return _errorRoute();
         }
         return MaterialPageRoute(
-          builder: (_) => ClinicDetailsScreen(clinicId: clinicId),
+          builder: (_) => BlocProvider(
+            create: (context) => di.sl<ClinicDetailsCubit>(),
+            child: ClinicDetailsScreen(clinicId: clinicId),
+          ),
         );
 
       default:
